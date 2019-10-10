a35fc48da7f646e1c7226e7605a882b426d08666
fastiz <facuastiz@gmail.com>
2019-10-10 18:13:26 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Random Wallpaper
+##Description
+Simple wallpaper changer programmed in python for Ubuntu 16.
+
+##How to use
+First fill paths.txt file with the paths of the directories containing the wallpapers. Where each line represents one directory.
+
+Finally run python ./main.py
+
+
---
+++ main.py
@@ -0,0 +1,45 @@
+import fnmatch
+import os
+import random
+
+
+def main():
+    try:
+        f = open("paths.txt", "r")
+        lines = f.read().splitlines()
+        f.close()
+    except:
+        print("Error occurred when reading paths.txt file")
+        return 1
+
+    wallpapers = []
+
+    allowed_file_types = ['*.png', '*.jpg', '*.jpeg']
+
+    for line in lines:
+
+        try:
+            entries = os.listdir(line)
+        except:
+            print('Wrong path (' + line + ')')
+            break
+
+        for entry in entries:
+            if any(fnmatch.fnmatch(entry, p) for p in allowed_file_types):
+                wallpapers.append(line + '/' + entry)
+
+    if len(wallpapers) == 0:
+        print('There are no wallpapers')
+        return 1
+
+    chosen_wallpaper = random.choice(wallpapers)
+
+    print('Changing wallpaper to: ' + chosen_wallpaper)
+
+    try:
+        os.system('gsettings set org.gnome.desktop.background picture-uri file://' + chosen_wallpaper)
+    except:
+        print('An error occurred when changing the wallpaper')
+
+
+main()
